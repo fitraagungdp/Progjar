3c3d84ae438bcc60862fac56eb244f875ff47630
fitraagungdp <103353262+fitraagungdp@users.noreply.github.com>
2023-03-27 07:05:19 +0700

    Update test.py

--- Tugas2/parser/test.py
+++ Tugas2/parser/test.py
@@ -1,41 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-class HTMLParser:
+class Parsing:
+    
     def __init__(self, url):
         self.url = url
         self.response = None
-        self.html_content = None
-        self.soup = None
+        self.container = None
+        self.Bsoup = None
         
-    def get(self):
+    def check(self):
         self.response = requests.get(self.url)
-        self.html_content = self.response.content
-        self.soup = BeautifulSoup(self.html_content, 'html.parser')
-     
-    def get_menu(self):
-        menu_list = self.soup.find(
-            'ul', {'class': 'navbar-nav h-100 wdm-custom-menus links'})
-        res = []
-        try:
-            li_menus = menu_list.find_all('li')
-            for menu in li_menus:
-                a = menu.find('a')
-                if a:
-                    res.append(a.text.strip())
-                    div = menu.find('div')
-                    inside_a = div.find_all('a')
-                    for inside in inside_a:
-                        res.append('\t' + inside.text.strip())
+        self.container = self.response.content
+        self.Bsoup = BeautifulSoup(self.container, 'html.parser')
         
+    def start(self):
+        list = self.Bsoup.find('ul',{'class': 'navbar-nav h-100 wdm-custom-menus links'})
+        respond = []
+        try:
+            findLi = list.find_all('li')
+            
+            for menu in findLi:
+                findA = menu.find('a')
+                
+                if findA:
+                    respond.append(findA.text.strip())
+                    findDiv = menu.find('div')
+                    inside_a = findDiv.find_all('a')
+                    for inside in inside_a:
+                        respond.append('\t' + inside.text.strip())
+                        
         except AttributeError:
             pass
-        return res
-    
+        return respond
+            
 if __name__ == "__main__":
-    html = HTMLParser('https://classroom.its.ac.id/')
-    html.get()
+    html = Parsing('https://classroom.its.ac.id/')
+    html.check()
     
-    print("Menu : ", html.get_menu())
-    for menu in html.get_menu():
+    # print("Menu : ", html.start())
+    for menu in html.start():
         print(menu)
